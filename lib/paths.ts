97d418cb6d100f8debc40e747073890fab1e061d
FylypekUNO <filip.dudzik.uno@gmail.com>
2024-11-27 19:42:58 +0100

    all paths in one file

---
+++ lib/paths.ts
@@ -0,0 +1,11 @@
+export const page = {
+  home: '/',
+  signIn: '/auth/signin',
+};
+
+export const api = {
+  signUp: '/api/auth/signup',
+};
+
+// For dynamic paths write using functions e.g.
+//  user: (id: string) => `/user/${id}`,
